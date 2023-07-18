9435b9108e69cec0afe3173582696450e9aa3191
tonildo123 <tony81191@gmail.com>
2023-07-18 16:48:30 -0300

    primer commit

---
+++ src/pages/terminos-y-condiciones/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TerminosCondiciones = () => {
+  return (
+    <div>
+      <h1>Términos y Condiciones</h1>
+      <p>
+        Aquí van los términos y condiciones de tu sitio web o aplicación. Puedes
+        agregar todo el texto relevante aquí.
+      </p>
+      <p>
+        Recuerda consultar con un abogado para asegurarte de que tus términos y
+        condiciones sean legales y apropiados para tu negocio.
+      </p>
+    </div>
+  );
+};
+
+export default TerminosCondiciones;
